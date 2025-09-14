89bc0666032053f61b7e27a3d2394cd4d0f25b21
Aerospace87 <gianlucapironti347@gmail.com>
2025-09-14 12:39:07 +0200

    Basic devops workflow

---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+General description of the code
+
+"""
+
+def add(x, y):
+    """Function to sum two numbers x and y"""
+    return x + y
+
+print(add(1, 1))
---
+++ test_hello.py
@@ -0,0 +1,6 @@
+#!/bin/usr/env python
+
+from hello import add
+
+def test_add():
+    assert add(1, 1) == 2
